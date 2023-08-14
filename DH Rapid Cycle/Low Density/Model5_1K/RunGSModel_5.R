a4084b463252ab1cb482e521d3f9b271f6735fc1
Guilherme de Jong <42737487+guilhermedejong@users.noreply.github.com>
2023-08-14 20:48:05 -0300

    Add files via upload

---
+++ DH Rapid Cycle/Low Density/Model5_1K/RunGSModel_5.R
@@ -0,0 +1,27 @@
+cat(" Collecting Training Population\n") 
+
+if (year >= 22){
+  Model234TrainPop = c(
+    Model234TrainPop, 
+    MaleHybridYT1,
+    MaleHybridYT2,
+    MaleHybridYT3,
+    FemaleHybridYT1,
+    FemaleHybridYT2,
+    FemaleHybridYT3
+  ) 
+  
+}
+
+
+if (year == 21) {
+  cat(" Running GS model\n")
+  SP$addSnpChip(100, force = TRUE)
+  gsModel = RRBLUP_SCA(Model234TrainPop, snpChip = 4)
+} else {
+  gsModel = RRBLUP_SCA2(Model234TrainPop, snpChip = 4,
+                        VuF = gsModel@Vu[1],
+                        VuM = gsModel@Vu[2],
+                        VuD = gsModel@Vu[3],
+                        Ve = gsModel@Ve)
+}
