51b7c65ed30f0093dc2af0fc08a417a7b31860ac
Guilherme de Jong <42737487+guilhermedejong@users.noreply.github.com>
2023-08-14 21:07:42 -0300

    Add files via upload

---
+++ DH Rapid Cycle/QTL/Model5_QTL/RunGSModel_5.R
@@ -0,0 +1,26 @@
+cat(" Collecting Training Population\n") 
+
+if (year >= 22){
+  Model234TrainPop = c(
+    Model234TrainPop, 
+    MaleHybridYT1,
+    MaleHybridYT2,
+    MaleHybridYT3,
+    FemaleHybridYT1,
+    FemaleHybridYT2,
+    FemaleHybridYT3
+  ) 
+  
+}
+
+
+if (year == 21) {
+  cat(" Running GS model\n")
+  gsModel = RRBLUP_SCA(Model234TrainPop, useQtl = TRUE)
+} else {
+  gsModel = RRBLUP_SCA2(Model234TrainPop, useQtl = TRUE,
+                        VuF = gsModel@Vu[1],
+                        VuM = gsModel@Vu[2],
+                        VuD = gsModel@Vu[3],
+                        Ve = gsModel@Ve)
+}
